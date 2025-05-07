8d2957f2558117f654606b8923d945e4f1882b16
c <7160844@qq.com>
2025-05-07 17:36:28 +0800

    Initial Commit

---
+++ noter PDF Reader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  noter PDF Reader
+//
+//  Created by c on 2025/5/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ noter PDF Reader/noter_PDF_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  noter_PDF_ReaderApp.swift
+//  noter PDF Reader
+//
+//  Created by c on 2025/5/7.
+//
+
+import SwiftUI
+
+@main
+struct noter_PDF_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
